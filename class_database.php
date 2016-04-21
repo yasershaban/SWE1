b979a5196ffa83598b55badeae8696f929874de0
TahaMgady <taha@taha-2.local>
2016-04-21 21:16:05 +0200

    Added attributes

--- class_database.php
+++ class_database.php
@@ -4,7 +4,11 @@ class class_database {
 
 
 // Attributes
-	
+	private $host = "localhost";
+	private $userName = "taha";
+	private $passwd = "estama";
+	private $databaseName = "SWE";
+	private $connectionl;
 
 	
 } // end class_database	
